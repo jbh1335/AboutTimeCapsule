80f1e29c55375328ebecc0090eae2c30fab23418
장별하 <daisy1080@naver.com>
2023-04-24 17:22:30 +0900

    [ADD] add BE/capsule-service directory

---
+++ BE/capsule-service/src/main/java/com/timecapsule/capsuleservice/CapsuleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.timecapsule.capsuleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapsuleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapsuleServiceApplication.class, args);
+	}
+
+}
