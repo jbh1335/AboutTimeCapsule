5a4261121f5ee74b11c21c6708cb03c4949aea45
ganjisriver <ganjisriver18@gmail.com>
2023-04-25 17:07:21 +0900

    [ADD] add login activity file

---
+++ FE/AboutTimeCapsule/app/src/main/java/com/aboutcapsule/android/views/login/LoginAcitivity.kt
@@ -0,0 +1,12 @@
+package com.aboutcapsule.android.views.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aboutcapsule.android.R
+
+class LoginAcitivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_acitivity)
+    }
+}
