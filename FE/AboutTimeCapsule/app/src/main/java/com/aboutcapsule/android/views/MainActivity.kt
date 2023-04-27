4e834e5377d9ce375854a8ec663df2173aa2455c
ganjisriver <ganjisriver18@gmail.com>
2023-04-27 16:47:20 +0900

    [ADD] add kakao login api

--- FE/AboutTimeCapsule/app/src/main/java/com/aboutcapsule/android/views/MainActivity.kt
+++ FE/AboutTimeCapsule/app/src/main/java/com/aboutcapsule/android/views/MainActivity.kt
@@ -2,11 +2,16 @@ package com.aboutcapsule.android.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.aboutcapsule.android.R
+import com.kakao.sdk.common.util.Utility
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val keyHash = Utility.getKeyHash(this)
+        Log.d("Hash", keyHash)
     }
 }
---
+++ FE/AboutTimeCapsule/app/src/main/java/com/aboutcapsule/android/views/login/GlobalAplication.kt
@@ -0,0 +1,14 @@
+package com.aboutcapsule.android.views.login
+
+import android.app.Application
+import com.aboutcapsule.android.BuildConfig
+import com.kakao.sdk.common.KakaoSdk
+
+class GlobalAplication:Application() {
+    override fun onCreate() {
+        super.onCreate()
+        KakaoSdk.init(this, BuildConfig.NATIVE_APP_KEY)
+    }
+
+
+}
