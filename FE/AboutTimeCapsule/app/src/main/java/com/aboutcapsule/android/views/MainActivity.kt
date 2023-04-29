6bba5d1e70f664a5695ab18cab13848abbb44fd7
bamm2 <xxknu88@naver.com>
2023-04-30 00:40:03 +0900

    [REFACTOR] jetpack Navigation +  data binding

--- FE/AboutTimeCapsule/app/src/main/java/com/aboutcapsule/android/views/MainActivity.kt
+++ FE/AboutTimeCapsule/app/src/main/java/com/aboutcapsule/android/views/MainActivity.kt
@@ -1,18 +1,14 @@
 package com.aboutcapsule.android.views
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import android.util.Log
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.aboutcapsule.android.R
 import com.aboutcapsule.android.databinding.ActivityMainBinding
-import com.aboutcapsule.android.views.chat.ChatMainFragment
-import com.aboutcapsule.android.views.mainpage.MainPageMainFragment
-import com.aboutcapsule.android.views.mainpage.MainPageMyCapsuleFragment
-import com.aboutcapsule.android.views.map.MapMainFragment
-import com.aboutcapsule.android.views.mypage.MyPageMainFragment
 
 private const val TAG_HOME = "main_page_fragment"
 private const val TAG_MAP = "map_fragment"
@@ -22,83 +18,29 @@ private const val TAG_MYPAGE = "my_page_fragment"
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-
-        // 최초렌더링 시 , 메인페이지로 세팅
-        setFragment(TAG_HOME, MainPageMainFragment())
-
-        binding.navigationView.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.mainPageFragment -> setFragment(TAG_HOME, MainPageMainFragment())
-
-                R.id.mapFragment -> setFragment(TAG_MAP, MapMainFragment())
-
-                R.id.chatFragment -> setFragment(TAG_CHAT, ChatMainFragment())
-
-                R.id.myPageFragment -> setFragment(TAG_MYPAGE, MyPageMainFragment())
-            }
-            true
-        }
+        initBinding()
+        initNavigation()
 
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
         val ab = supportActionBar!!
         ab.setDisplayShowTitleEnabled(false)
 
-        setContentView(binding.root)
     }
 
-    private fun setFragment(tag: String, fragment: Fragment) {
-        val manager: FragmentManager = supportFragmentManager
-        val fragTransaction = manager.beginTransaction()
-
-        if (manager.findFragmentByTag(tag) == null) {
-            fragTransaction.add(R.id.mainFrameLayout, fragment, tag)
-        }
-
-        val home = manager.findFragmentByTag(TAG_HOME)
-        val map = manager.findFragmentByTag(TAG_MAP)
-        val chat = manager.findFragmentByTag(TAG_CHAT)
-        val myPage = manager.findFragmentByTag(TAG_MYPAGE)
-
-        if (home != null) { fragTransaction.hide(home) }
-        if (map != null) { fragTransaction.hide(map) }
-        if (chat != null) { fragTransaction.hide(chat) }
-        if (myPage != null) { fragTransaction.hide(myPage) }
-
-        if (tag == TAG_HOME) {
-            if (home != null) {
-                fragTransaction.show(home)
-            }
-        } else if (tag == TAG_MAP) {
-            if (map != null) {
-                fragTransaction.show(map)
-            }
-        } else if (tag == TAG_CHAT) {
-            if (chat != null) {
-                fragTransaction.show(chat)
-            }
-        } else if (tag == TAG_MYPAGE) {
-            if (myPage != null) {
-                fragTransaction.show(myPage)
-            }
-        }
-
-        fragTransaction.commitAllowingStateLoss()
+    private fun initNavigation() {
+        val navController = findNavController(R.id.nav_host_fragment)
+        binding.navBottom.setupWithNavController(navController)
     }
 
-    fun replaceFragment(fragment: Fragment) {
-        Log.d("페이지리다이렉트", "메인 액티비티 ")
-        Log.d("페이지리다이렉트", "${fragment}")
-
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.mainFrameLayout,ChatMainFragment()).commitAllowingStateLoss();
+    private fun initBinding(){
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.lifecycleOwner = this
     }
 
 }
--- FE/AboutTimeCapsule/app/src/main/java/com/aboutcapsule/android/views/mainpage/CustomDialogMainpage.kt
+++ FE/AboutTimeCapsule/app/src/main/java/com/aboutcapsule/android/views/mainpage/CustomDialogMainpage.kt
@@ -3,32 +3,29 @@ package com.aboutcapsule.android.views.mainpage
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
 import androidx.fragment.app.DialogFragment
-import com.aboutcapsule.android.R
 import com.aboutcapsule.android.databinding.FragmentCustomDialogMainpageBinding
 
 
 class CustomDialogMainpage : DialogFragment() {
-    private var _binding: FragmentCustomDialogMainpageBinding? = null
-    private val binding get() = _binding!!
+    private var binding : FragmentCustomDialogMainpageBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentCustomDialogMainpageBinding.inflate(inflater,container,false)
-        val view = binding.root
+        binding = FragmentCustomDialogMainpageBinding.inflate(inflater,container,false)
+        val view = binding?.root
 
 //      테두리 둥글게 만들기 위한 설정
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         dialog?.requestWindowFeature(Window.FEATURE_NO_TITLE)
 
-        binding.mainDialogCloseBtn.setOnClickListener {
+        binding?.mainDialogCloseBtn!!.setOnClickListener {
             dismiss()
         }
 
@@ -37,7 +34,7 @@ class CustomDialogMainpage : DialogFragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
+        binding = null
     }
 
 }
