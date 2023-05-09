62c2506f47114a4283ba270fcd46864b69b5615a
장별하 <daisy1080@naver.com>
2023-05-09 11:30:31 +0900

    [ADD] FcmConfig

---
+++ BE/capsule-service/src/main/java/com/timecapsule/capsuleservice/config/FcmConfig.java
@@ -0,0 +1,30 @@
+package com.timecapsule.capsuleservice.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FcmConfig {
+    @Value("${firebase.key.path}")
+    private String firebaseSdkPath;
+
+    @PostConstruct
+    public void init() {
+        try {
+            FileInputStream serviceAccount = new FileInputStream(firebaseSdkPath);
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+            FirebaseApp.initializeApp(options);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
