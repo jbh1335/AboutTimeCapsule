feb2d2719f26bcc7e522b0410cf1fd1a15ddf68d
김아린 <mwtgal20@gmail.com>
2023-05-09 09:18:15 +0900

    [UPDATE] WebSocketConfig endpoint 변경

--- BE/chat-service/src/main/java/com/timecapsule/chatservice/config/WebSocketConfig.java
+++ BE/chat-service/src/main/java/com/timecapsule/chatservice/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stomp/chat")
+        registry.addEndpoint("/ws-stomp") // ws://localhost:9004/ws-stomp
                 .setAllowedOriginPatterns("*").withSockJS();
     }
 }
