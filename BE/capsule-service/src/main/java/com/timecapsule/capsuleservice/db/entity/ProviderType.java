355abfc52d7a14a8d264e669c7f176090de42940
장별하 <daisy1080@naver.com>
2023-05-12 16:14:47 +0900

    [UPDATE] ProviderType 수정

--- BE/capsule-service/src/main/java/com/timecapsule/capsuleservice/db/entity/ProviderType.java
+++ BE/capsule-service/src/main/java/com/timecapsule/capsuleservice/db/entity/ProviderType.java
@@ -1,6 +1,5 @@
 package com.timecapsule.capsuleservice.db.entity;
 
 public enum ProviderType {
-    GOOGLE,
-    KAKAO
+    NAVER, KAKAO
 }
--- BE/capsule-service/src/main/java/com/timecapsule/capsuleservice/service/RedisService.java
+++ BE/capsule-service/src/main/java/com/timecapsule/capsuleservice/service/RedisService.java
@@ -1,5 +1,27 @@
-package com.timecapsule.capsuleservice.service;
-
-public class RedisService {
-
-}
+//package com.timecapsule.capsuleservice.service;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.data.redis.core.HashOperations;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class RedisService {
+//    private final RedisTemplate<String, Object> redisTemplate;
+//    public void setData(String key, Object field, Object value) {
+//        HashOperations<String, Object, Object> hashOperations = redisTemplate.opsForHash();
+//        System.out.println("key: " + key + " field: " + field + " value: " + value);
+//        hashOperations.put(key, field, value);
+//    }
+//
+//    public Object getDataValue(String key, Object field) {
+//        HashOperations<String, Object, Object> hashOperations = redisTemplate.opsForHash();
+//        return hashOperations.get(key, field);
+//    }
+//
+//    public void deleteData(String key, Object field) {
+//        HashOperations<String, Object, Object> hashOperations = redisTemplate.opsForHash();
+//        hashOperations.delete(key, field);
+//    }
+//}
