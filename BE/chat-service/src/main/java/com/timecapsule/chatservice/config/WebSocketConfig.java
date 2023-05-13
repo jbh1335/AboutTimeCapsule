ce5d93bc96c0ae2409821eb0d937c211b5b6424a
김아린 <mwtgal20@gmail.com>
2023-05-14 01:03:16 +0900

    [UPDATE] WebSocketConfig allowedOrigins 명시

--- BE/chat-service/src/main/java/com/timecapsule/chatservice/config/WebSocketConfig.java
+++ BE/chat-service/src/main/java/com/timecapsule/chatservice/config/WebSocketConfig.java
@@ -9,7 +9,6 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
@@ -20,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp") // ws://localhost:9004/ws-stomp
-                .setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/stomp/chat") // ws://k8b302.p.ssafy.io/stomp/chat
+                .setAllowedOrigins("http://k8b302.p.ssafy.io").withSockJS();
     }
 }
