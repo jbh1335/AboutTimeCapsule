ce0b18bc6f35edce6c8d0b445229ed4b5cb79bda
장별하 <daisy1080@naver.com>
2023-05-19 02:57:45 +0900

    [UPDATE] log

--- BE/capsule-service/src/main/java/com/timecapsule/capsuleservice/service/CapsuleServiceImpl.java
+++ BE/capsule-service/src/main/java/com/timecapsule/capsuleservice/service/CapsuleServiceImpl.java
@@ -8,6 +8,7 @@ import com.timecapsule.capsuleservice.dto.*;
 import com.timecapsule.capsuleservice.exception.CustomException;
 import com.timecapsule.capsuleservice.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -28,6 +29,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
+@Slf4j
 @Service("capsuleService")
 @RequiredArgsConstructor
 public class CapsuleServiceImpl implements CapsuleService {
@@ -542,6 +544,7 @@ public class CapsuleServiceImpl implements CapsuleService {
                     capsule.getLatitude(), capsule.getLongitude(), "meter");
             boolean isAllowedDistance = (distance <= 100);
 
+            log.info("capsuleId: {} {} {}", capsule, capsule.getTitle(), isLocked);
             mapResList.add(MapRes.builder()
                     .capsuleId(capsule.getId())
                     .isLocked(isLocked)
